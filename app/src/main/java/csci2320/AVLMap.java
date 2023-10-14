f8e05f022ebfb5dde696ce4c31b14692dfffc28e
Mark Lewis <mlewis@trinity.edu>
2023-10-14 14:32:50 -0500

    Initial commit.

---
+++ app/src/main/java/csci2320/AVLMap.java
@@ -0,0 +1,211 @@
+package csci2320;
+
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class AVLMap<K extends Comparable<K>, V> implements Map<K, V> {
+  // Put your Node class and private data up here.
+
+  // I'm giving you some helper method to make testing easier.
+  @SuppressWarnings("unchecked")
+  public static <K extends Comparable<K>, V> AVLMap<K, V> of(Map.KeyValuePair<K, V>... elems) {
+    AVLMap<K, V> ret = new AVLMap<>();
+    for (var kvp: elems) ret.put(kvp.key(), kvp.value());
+    return ret;
+  }
+
+  @Override
+  public boolean equals(Object that) {
+    if (that == null || !(that instanceof AVLMap)) return false;
+    AVLMap<?, ?> thatSeq = (AVLMap<?, ?>)that;
+    if (thatSeq.size() != size()) return false;
+    for (Iterator<?> iter1 = thatSeq.iterator(), iter2 = this.iterator(); iter1.hasNext();)
+      if (!iter1.next().equals(iter2.next())) return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("BSTMap(");
+    boolean first = true;
+    for (var kvp: this) {
+      if (!first) {
+        sb.append(", " + kvp.key() +"->"+kvp.value());
+      } else {
+        sb.append(kvp.key() +"->"+kvp.value());
+        first = false;
+      }
+    }
+    sb.append(")");
+    return sb.toString();
+  }
+
+  /**
+   * Return the height of the tree. This is just for testing purposes, but my tests are going to call it.
+   * @return The height of the tree.
+   */
+  public int height() {
+    // return root.height;  // Suggested implementation.
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+  }
+
+  @Override
+  public Iterator<KeyValuePair<K, V>> iterator() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+
+    // Note that if you choose to put a parent in your node, you can use the pseudocode
+    // in the book for this. If you don't, you are allowed to use a java.util.Stack. Both
+    // approaches will need to be named so they can have a constructor that either moves
+    // the Node pointer to the first value or populates the stack.
+  }
+
+  @Override
+  public Optional<V> put(K key, V value) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'put'");
+  }
+
+  @Override
+  public Optional<V> get(K key) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'get'");
+  }
+
+  @Override
+  public V getOrElse(K key, V defaultValue) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getOrElse'");
+  }
+
+  @Override
+  public boolean contains(K key) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'contains'");
+  }
+
+  @Override
+  public Optional<V> remove(K key) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'remove'");
+  }
+
+  @Override
+  public int size() {
+    // TODO basic implementation to get test to pass.
+    return 0;
+  }
+
+  @Override
+  public Set<K> keySet() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'keySet'");
+
+    // Implementation note. This can be done with an anonymous inner class.
+    // You can refer to the BSTMap it is in with `BSTMap.this`. So you
+    // can call things like `BSTMap.this.contains`.
+  }
+  
+  @Override
+  public <V2> AVLMap<K, V2> mapValues(Function<V, V2> f) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'mapValues'");
+  }
+
+  @Override
+  public Map<K, V> filter(Function<Map.KeyValuePair<K, V>, Boolean> predicate) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'filter'");
+  }
+
+  @Override
+  public Optional<Map.KeyValuePair<K, V>> find(Function<Map.KeyValuePair<K, V>, Boolean> predicate) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'find'");
+  }
+
+  @Override
+  public <E2> E2 fold(E2 zero, BiFunction<E2, Map.KeyValuePair<K, V>, E2> f) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'fold'");
+  }
+
+  @Override
+  public boolean exists(Function<Map.KeyValuePair<K, V>, Boolean> predicate) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'exists'");
+  }
+
+  @Override
+  public boolean forall(Function<Map.KeyValuePair<K, V>, Boolean> predicate) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'forall'");
+  }
+
+  /**
+   * Lets this object work as a function from K to V. Gets the value if the key exists.
+   * Throws an exception if it doesn't.
+   * @param key the key to look up in the collection
+   * @return the associated value
+   */
+  @Override
+  public V apply(K key) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'apply'");
+  }
+
+  // ----------------------------These are potential helper functions for debugging ---------------------
+  // You can modify these if you want. I don't call them in any way. They don't have to be used if you 
+  // don't need them. I used them in debugging my own implementation.
+
+  // public void inorderPrint() {
+  //   inorderPrintRecur(root);
+  // }
+
+  // private void inorderPrintRecur(Node<K, V> n) {
+  //   if (n != null) {
+  //     inorderPrintRecur(n.left);
+  //     System.out.println(n.key+" -> "+n.value+"  ");
+  //     inorderPrintRecur(n.right);
+  //   }
+  // }
+
+  // public void preorderPrintKeys() {
+  //   preorderPrintKeysRecur(root);
+  //   System.out.println();
+  // }
+
+  // public void preorderPrintKeysRecur(Node<K, V> n) {
+  //   if (n != null) {
+  //     System.out.print("(" + n.key);
+  //     preorderPrintKeysRecur(n.left);
+  //     preorderPrintKeysRecur(n.right);
+  //     System.out.print(")");
+  //   }
+  // }
+
+  // public boolean isConsistent() {
+  //   return isConsistentRecur(root);
+  // }
+
+  // private boolean isConsistentRecur(Node<K, V> n) {
+  //   if (n == null) return true;
+  //   if (n.parent != null && n.parent.left != n && n.parent.right != n) {
+  //     System.out.println("Not a child of parent at "+n.key);
+  //     return false;
+  //   }
+  //   if (n.left != null && n.left.key.compareTo(n.key) >= 0) {
+  //     System.out.println("Left child not smaller at "+n.key);
+  //     return false;
+  //   }
+  //   if (n.right != null && n.right.key.compareTo(n.key) <= 0) {
+  //     System.out.println("Right child not larger at "+n.key);
+  //     return false;
+  //   }
+  //   return isConsistentRecur(n.left) && isConsistentRecur(n.right);
+  // }
+
+}
---
+++ app/src/test/java/csci2320/AVLMapTest.java
@@ -0,0 +1,109 @@
+package csci2320;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class AVLMapTest {
+  @Test void emptyOnCreate() {
+    Map<String, Integer> map = new AVLMap<>();
+    assertEquals(0, map.size());
+  }
+
+  /**
+   * Leave this test here! It is verifying the AVL is doing the right thing.
+   */
+  @Test void heightTest1() {
+    AVLMap<Integer, Integer> map = new AVLMap<>();
+    for (int i = 0; i < (1 << 20) - 1; ++i) {
+      map.put(i, i);
+    }
+    assertEquals(20, map.height());
+  }
+
+  /**
+   * Leave this test here! It is verifying the AVL is doing the right thing.
+   */
+  @Test void heightTest2() {
+    AVLMap<Integer, Integer> map = new AVLMap<>();
+    for (int i = (1 << 20) - 1; i > 0; ++i) {
+      map.put(i, i);
+    }
+    assertEquals(20, map.height());
+  }
+
+  /**
+   * Leave this test here! It is verifying the AVL is doing the right thing.
+   */
+  @Test void heightTest3() {
+    AVLMap<Integer, Integer> map = new AVLMap<>();
+    var mid = (1 << 19);
+    map.put(mid, mid);
+    for (int i = 1; i < (1 << 20) - 1; ++i) {
+      map.put(mid - i,mid - i);
+      map.put(mid + i,mid + i);
+    }
+    assertEquals(20, map.height());
+  }
+
+  /**
+   * Leave this test here! It is verifying the AVL is doing the right thing.
+   */
+  @Test void heightTest4() {
+    AVLMap<Integer, Integer> map = new AVLMap<>();
+    var mid = (1 << 19);
+    map.put(mid, mid);
+    for (int i = 1; i < (1 << 20) - 1; ++i) {
+      map.put(i, i);
+      map.put((1 << 20) - i,(1 << 20) + i);
+    }
+    assertEquals(20, map.height());
+  }
+
+  // I'm giving you this test of your iterator that does a petty good job of finding
+  // standard errors.
+  // @Test void testIterator() {
+  //   Map<String, Integer> map = new BSTMap<>();
+  //   assertEquals(0, map.size());
+  //   var keys = List.of("abc", "xyz", "def", "bob", "pat");
+  //   var vals = List.of(3, 19, 42, 16, 32);
+  //   for (int i = 0; i < keys.size(); ++i) {
+  //     map.put(keys.get(i), vals.get(i));
+  //   }
+  //   assertEquals(keys.size(), map.size());
+  //   int cnt = 0;
+  //   for (var kvp: map) {
+  //     int index = keys.indexOf(kvp.key());
+  //     assertEquals(kvp.value(), vals.get(index));
+  //     cnt++;
+  //     assertTrue(cnt <= keys.size()); // This prevents you from going into an infinite loop if your iterator doesn't stop.
+  //   }
+  //   assertEquals(keys.size(), cnt);
+  // }
+
+  // Testing remove can be hard. This does a randomized test of remove. You should write your own little
+  // tests, but 
+  // @Test void testRemoveBig() {
+  //   BSTMap<String, Integer> map = new BSTMap<>();
+  //   assertEquals(0, map.size());
+  //   var keys = new ArrayList<String>();
+  //   var vals = new ArrayList<Integer>();
+  //   var rand = new Random();
+  //   for (int i = 0; i < 1000; ++i) {
+  //     keys.add(App.randomString(rand, 10));
+  //     vals.add(rand.nextInt());
+  //     map.put(keys.get(i), vals.get(i));
+  //   }
+  //   Collections.shuffle(keys);
+  //   for (int i = 0; i < keys.size(); ++i) {
+  //     var key = keys.get(i);
+  //     var value = map.get(key).get();
+  //     assertTrue(map.contains(key));
+  //     assertEquals(Optional.of(value), map.remove(key));
+  //     map.preorderPrintKeys();
+  //     assertTrue(map.isConsistent());
+  //     assertFalse(map.contains(key));
+  //     assertEquals(keys.size()-i-1, map.size());
+  //   }
+  // }
+
+}
